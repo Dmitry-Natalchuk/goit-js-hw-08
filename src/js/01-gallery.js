76676c01dc18aff4491a4ebf942e507c36704e0c
Dmitry Natalchuk <next1996@ukr.net>
2022-07-18 13:59:38 +0300

    push gallery add npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Change code below this line
+//console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const imagesList = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend",imagesList);
+
+function createGallery(images) {
+  return galleryItems.map(({preview, original, description }) => {
+   return` <a class="gallery__item"
+    href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`;})
+.join("")
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    //captionType : "alt",
+    captionsData: "alt",
+   // docClose: false,
+   // close : false,
+    captionDelay: 250,
+    animationSpeed : 200,
+    maxZoom: 5,
+   // disableScroll : false,
+    
+  });
+
