fd55e6e3b459018562a1be18ca4a8dacdb64d5a8
Dmitry-Natalchuk <95769351+Dmitry-Natalchuk@users.noreply.github.com>
2022-07-21 13:13:56 +0000

    Deploying to gh-pages from @ Dmitry-Natalchuk/goit-js-hw-08@e374f60ed7b3b017a8e6bb0fa86824ad71f38836 🚀

--- 03-feedback.593be5f3.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.querySelector("input"),e.querySelector("textarea"),e.querySelector("button");
-//# sourceMappingURL=03-feedback.593be5f3.js.map
--- 03-feedback.a1804bca.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");e.querySelector("input"),e.querySelector("textarea"),e.querySelector("button")}();
-//# sourceMappingURL=03-feedback.a1804bca.js.map
